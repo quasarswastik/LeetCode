58812ba2de128e7b7685174e90a5f5bb2aa6cc6b
quasarswastik <quasarswastik@gmail.com>
2022-06-26 16:30:26 +0800

    Create two_sum.py
    Time: O(n)
    Space: O(n)

---
+++ two_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        number_indices = {}
+        for i in range(len(nums)):
+            if target - nums[i] in number_indices:
+                return number_indices[target - nums[i]], i
+            number_indices[nums[i]] = i
